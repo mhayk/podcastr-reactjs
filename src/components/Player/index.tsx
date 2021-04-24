ffd0920ef399ae0b9ee550c11445aed5b269223a
Mhayk Whandson <hi@mhayk.com>
2021-04-24 15:54:50 +0100

    Package rc-slider has been installed and it has been styled.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import { useContext } from 'react';
 import { PlayerContext } from '../../contexts/PlayerContext';
+import Slider from 'rc-slider'
+
+import 'rc-slider/assets/index.css'
+
 import styles from './styles.module.scss';
 
 export function Player() {
@@ -33,11 +37,20 @@ export function Player() {
             )
             }
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+                        {episode ? (
+                            <Slider
+                                trackStyle={{ backgroundColor: '#04d361' }}
+                                railStyle={{ backgroundColor: '#9f75ff' }}
+                                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+                            />
+                        ) : (
+                            <div className={styles.emptySlider} />
+                        )}
+
                     </div>
                     <span>00:00</span>
                 </div>
