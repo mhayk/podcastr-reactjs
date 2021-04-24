14c0253ccde56a263393fda35236cfcd1570f83b
Mhayk Whandson <hi@mhayk.com>
2021-04-24 20:23:24 +0100

    Shuffle has been implemented.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -13,12 +13,14 @@ type PlayerContextData = {
     currentEpisodeIndex: number;
     isPlaying: boolean;
     isLooping: boolean;
+    isShuffling: boolean;
     play: (episode: Episode) => void;
     playNext: () => void;
     playPrevious: () => void;
     playList: (list: Episode[], index: number) => void;
     togglePlay: () => void;
     toggleLoop: () => void;
+    toggleShuffle: () => void;
     setPlayingState: (state: boolean) => void;
     hasNext: boolean;
     hasPrevious: boolean;
@@ -35,6 +37,7 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
     const [isLooping, setIsLooping] = useState(false);
+    const [isShuffling, setIsShuffling] = useState(false);
 
     function play(episode: Episode) {
         setEpisodeList([episode]);
@@ -56,6 +59,10 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         setIsLooping(!isLooping)
     }
 
+    function toggleShuffle() {
+        setIsShuffling(!isShuffling)
+    }
+
     function setPlayingState(state: boolean) {
         setIsPlaying(state)
     }
@@ -65,7 +72,10 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
 
     function playNext() {
 
-        if (hasNext) {
+        if (isShuffling) {
+            const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+            setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+        } else if (hasNext) {
             setCurrentEpisodeIndex(currentEpisodeIndex + 1)
         }
     }
@@ -82,9 +92,11 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
                 currentEpisodeIndex,
                 isPlaying,
                 isLooping,
+                isShuffling,
                 play,
                 togglePlay,
                 toggleLoop,
+                toggleShuffle,
                 setPlayingState,
                 playList,
                 playNext,
